8202ba168b8af5515dad9efbba2e8b25eb2f71b8
Sayanjit <sayanjitjana373@gmail.com>
2026-06-13 13:51:39 +0530

    Modules, Comments & Pip

---
+++ module.py
@@ -0,0 +1,22 @@
+import pyjokes
+
+# print("Printing Jokes...")
+
+# This prints a random joke
+joke = pyjokes.get_joke()
+print(joke)
+
+
+'''This is a multi-line comment.
+we can use triple single quotes for multi-line comments.'''
+
+"""This is another multi-line comment.
+We can also use triple double quotes for multi-line comments."""
+
+''' 
+Modules : A module is a file containing code written by somebody else which can be imported and used in our
+programs.
+
+Pip : Pip is the package manager for Python. You can use pip to install external modules on your system.
+pip install flask
+'''
