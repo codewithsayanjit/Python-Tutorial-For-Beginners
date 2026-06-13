7546fa66994d4899f565a92aa7eadaccbecd5b9c
Sayanjit <sayanjitjana373@gmail.com>
2026-06-13 14:34:32 +0530

    Datatype in Python refers to the classification of data items. It defines the type of value a variable can hold.

---
+++ Variables and Datatype/datatypes.py
@@ -0,0 +1,9 @@
+a = 10 # a is an integer.It is a whole number that can be positive, negative, or zero. Integers are used to represent quantities, counts, or any other type of numerical data that does not require decimal places.
+
+b = 3.71 # b is a floating point number.It is a number that has a decimal point. Floating point numbers are used to represent quantities that can have fractional parts.
+
+c = "Sayanjit" # c is a string.It is a sequence of characters enclosed in quotes. Strings can be used to represent text, such as names, addresses, or any other type of information that can be expressed in words.
+
+d = False # d is a boolean variable.It can have only two values: True or False. It is often used in conditional statements to control the flow of a program.
+
+e = None # e is a none type variable.It is used to represent the absence of a value or a null value. It is often used as a placeholder for variables that have not been assigned a value yet.
