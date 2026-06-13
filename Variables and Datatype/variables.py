aabc96e5dbd426a72111b9e094e4a1f7bbf0a0e6
Sayanjit <sayanjitjana373@gmail.com>
2026-06-13 14:29:17 +0530

    Variables

---
+++ Variables and Datatype/variables.py
@@ -0,0 +1,5 @@
+# A variable is the name given to a memory location in a program. For example.
+
+a = 07 # variables = container to store a value
+b = "Sayanjit" # keywords = reserved words in python
+c = 71.22 # identifiers = class/function/variable name
