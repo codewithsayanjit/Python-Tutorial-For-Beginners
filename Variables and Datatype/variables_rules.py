c79618f63de60c200e17978c815e9687b8ec05a5
Sayanjit <sayanjitjana373@gmail.com>
2026-06-13 14:43:53 +0530

    Variables Rules

---
+++ Variables and Datatype/variables_rules.py
@@ -0,0 +1,14 @@
+a = 23
+aaa = 435
+_sayanjit = 34
+
+# @sameer = 56 # Invalid due to @ symbol
+# s@meer # Invalid due to @ symbol
+'''
+Rules for Choosing an Identifier : 
+1. A variable name can contain alphabets, digits, and underscores.
+2. A variable name can only start with an alphabet and underscores.
+3. A variable name can’t start with a digit.
+4. No white space is allowed to be used inside a variable name.
+Examples of a few variable names are: one8, seven_, _seven etc.
+'''
