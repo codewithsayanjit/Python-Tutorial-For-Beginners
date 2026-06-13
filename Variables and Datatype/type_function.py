17353a017c70f9f889700456ed46eb5379013f23
Sayanjit <sayanjitjana373@gmail.com>
2026-06-13 21:27:55 +0530

    Type Function is used to find the data type of a given variable in python.

---
+++ Variables and Datatype/type_function.py
@@ -0,0 +1,17 @@
+# type() function is used to find the data type of a given variable in python.
+
+a = "31.2"
+b = float(a) # a converted to float
+t = type(a)
+print(t)
+print(type(b))
+
+
+c = 31
+type(c) # class <int>
+d = "31"
+type(d) # class <str>
+print(type(c))
+print(type(d))
+
+# A number can be converted into a string and vice versa (if possible)
