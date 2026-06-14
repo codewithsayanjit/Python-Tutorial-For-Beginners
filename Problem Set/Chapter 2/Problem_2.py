08997e0eff88bbd0c030cf36736e7668cc9b7045
Sayanjit <sayanjitjana373@gmail.com>
2026-06-14 16:33:48 +0530

    Write a python program to find remainder when a number is divided by z.

---
+++ Problem Set/Chapter 2/Problem_2.py
@@ -0,0 +1,7 @@
+# Write a python program to find remainder when a number is divided by z.
+
+a = 43
+
+b = 5
+
+print("Remainder when a is divided by b is", a % b) # "%" is the modulus operator which gives the remainder of the division.
