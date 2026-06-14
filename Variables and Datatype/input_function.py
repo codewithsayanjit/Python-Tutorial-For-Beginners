e0868c311afe4e61700b9458addfe620a48d6bf9
Sayanjit <sayanjitjana373@gmail.com>
2026-06-14 16:25:32 +0530

    Input Function in Python

---
+++ Variables and Datatype/input_function.py
@@ -0,0 +1,9 @@
+# This input function allows the user to take input from the keyboard as a string. The input() function takes the input as a string and returns it. If we want to take input as an integer or float, we need to convert it using int() or float() functions respectively.
+
+a = int(input("Enter number 1: "))
+
+b = int(input("Enter number 2: "))
+
+print("Number a is: ", a)
+print("Number b is: ", b) 
+print("Sum is ", a + b)
