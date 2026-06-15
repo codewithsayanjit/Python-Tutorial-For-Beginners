dd664156c4852caecc8b95c4b965e115f4c83553
Sayanjit <sayanjitjana373@gmail.com>
2026-06-15 12:11:04 +0530

    Slicing with Skip Value.

---
+++ String/String_Slice_Skip_Value.py
@@ -0,0 +1,9 @@
+# Slicing with Skip Value. We can provide a skip value as a part of our slice like this:
+word = "amazing"
+word[1:6:2] # mzn . [start:stop:skip_value ]
+
+# Other advanced slicing techniques:
+word = "amazing"
+word[-7:-1] # amazin
+word[:7] # amazing
+word[0:] # amazing
