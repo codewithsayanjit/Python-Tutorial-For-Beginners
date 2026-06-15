1e967c8c902d7d74fdeaa558c3151b13f153ab3d
Sayanjit <sayanjitjana373@gmail.com>
2026-06-15 17:51:20 +0530

    A tuple is an immutable data type in python.

---
+++ Lists And Tuples/Tuple.py
@@ -0,0 +1,9 @@
+# A tuple is an immutable data type in python. It is similar to a list but cannot be modified after creation. Once a tuple is created, you cannot add, remove, or change its elements. Tuples are defined using parentheses () and can contain elements of different data types.
+
+a = (1,45,342,3424,False, "Rohan", "Shivam")
+print(a)
+print(type(a))
+
+a1 = (1,) # tuple with only one element needs a comma
+print(a1)
+print(type(a1))
