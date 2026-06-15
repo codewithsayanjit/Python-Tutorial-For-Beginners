2eecc8e6f957680a7a7dfbea554bc755b1cff869
Sayanjit <sayanjitjana373@gmail.com>
2026-06-15 16:47:00 +0530

    Write a program to format the following letter using escape sequence characters.

---
+++ Problem Set/Chapter 3/Problem_5.py
@@ -0,0 +1,8 @@
+'''
+Write a program to format the following letter using escape sequence characters.
+letter = "Dear Sayanjit, this python course is nice. Thanks!"
+'''
+
+letter = "Dear Sayanjit,\n\tThis python course is nice.\nThanks!"
+
+print(letter)
