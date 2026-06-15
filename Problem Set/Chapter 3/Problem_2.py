e2b69f6f38ff31fda5594681cfa7b1fe0ec191aa
Sayanjit <sayanjitjana373@gmail.com>
2026-06-15 16:39:31 +0530

    Write a program to fill in a letter template given below with name and date.

---
+++ Problem Set/Chapter 3/Problem_2.py
@@ -0,0 +1,7 @@
+# Write a program to fill in a letter template given below with name and date.
+
+letter = '''Dear <|Name|>, 
+You are selected! 
+<|Date|> '''
+
+print(letter.replace("<|Name|>", "Sayanjit").replace("<|Date|>", "15 June 2026"))
