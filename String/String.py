43e5c0113418ffa13af2d4e73d597f6554897dd6
Sayanjit <sayanjitjana373@gmail.com>
2026-06-15 11:46:13 +0530

    String is a data type in python.

---
+++ String/String.py
@@ -0,0 +1,7 @@
+# String is a data type in python. String is a sequence of characters enclosed in quotes. We can primarily write a string in these three ways.
+
+a = 'Sayanjit' # Single quoted string.It uses single quotes to enclose the string. It is the most common way to write a string in python.
+
+b = "Sayanjit" # Double quoted string.It uses double quotes to enclose the string. It is also a common way to write a string in python. It is useful when we want to include single quotes in the string.
+
+c = '''Sayanjit''' # Triple quoted string.It uses triple quotes to enclose the string. It is useful when we want to include both single and double quotes in the string. It can also be used to write multi-line strings.
