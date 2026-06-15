9556116801f60b32df309fa49d5dbf0813899985
Sayanjit <sayanjitjana373@gmail.com>
2026-06-15 12:41:33 +0530

    Escape Sequence Characters in Python

---
+++ String/Escape_Sequence_Characters.py
@@ -0,0 +1,16 @@
+'''
+Escape Sequence Characters
+Sequence of characters after backslash "\" are called Escape Sequence characters.
+These characters represent one special character inside strings.
+Escape Sequence Characters in Python:
+1. \n : It is used to insert a new line in the string.
+2. \t : It is used to insert a tab in the string.
+3. \\ : It is used to insert a backslash in the string.
+4. \" : It is used to insert a double quote in the string.
+5. \' : It is used to insert a single quote in the string.
+6. \r : It is used to insert a carriage return in the string.
+7. \b : It is used to insert a backspace in the string.
+8. \f : It is used to insert a form feed in the string.
+9. \v : It is used to insert a vertical tab in the string.
+10. \0 : It is used to insert a null character in the string.
+'''
