0fc37b0cadc2d01f2f8cc76e7368c4e494349dbd
Sayanjit <sayanjitjana373@gmail.com>
2026-06-15 16:44:45 +0530

    Replace the double space from problem 3 with single spaces.

---
+++ Problem Set/Chapter 3/Problem_4.py
@@ -0,0 +1,6 @@
+# Replace the double space from problem 3 with single spaces.
+
+name = "Sayanjit is a good  boy and  he is a good  programmer."
+
+print(name.replace("  ", " "))
+print(name) # Strings are immutable which means that you cannot change them by running functions on them. 
