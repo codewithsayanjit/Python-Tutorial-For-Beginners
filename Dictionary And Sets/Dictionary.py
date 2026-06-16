2944a8047b16a7ee89f747b617f89cb805b39bd7
Sayanjit <sayanjitjana373@gmail.com>
2026-06-16 17:56:34 +0530

    Dictionary is a collection of keys-value pairs.

---
+++ Dictionary And Sets/Dictionary.py
@@ -0,0 +1,21 @@
+# Dictionary is a collection of keys-value pairs.
+'''
+Properties Of Python Dictionaries
+1)It is unordered.
+2)It is mutable.
+3)It is indexed.
+4)It Cannot contain duplicate keys.
+'''
+
+
+d = {} # Empty dictionary
+marks = {
+    "Sayanjit": 100,
+    "Shubham": 56,
+    "Rohan": 32,
+    25 : "Dipayan"
+}
+
+
+print(marks, type(marks))
+print(marks["Sayanjit"])
