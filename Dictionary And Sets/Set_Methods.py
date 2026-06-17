52cd52b61b59d117857a860ca8c484ac181c3e01
Sayanjit <sayanjitjana373@gmail.com>
2026-06-17 16:34:05 +0530

    Set Methods in Python

--- Dictionary And Sets/Set_Methods.py
+++ Dictionary And Sets/Set_Methods.py
@@ -10,6 +10,12 @@ print("Union:", s.union({7, 8})) # Union of sets combines all unique elements fr
 print("Intersection:", s.intersection({3, 4, 9})) # Intersection of sets returns only the elements that are present in both sets
 print("Difference:", s.difference({3, 4})) # Difference of sets returns elements that are in the first set but not in the second set
 print("Symmetric Difference:", s.symmetric_difference({4, 5, 9})) # Symmetric difference of sets returns elements that are in either set but not in both
-print("Subset:", {1}.issubset(s)) # Check if the first set is a subset of the second set
-print("Superset:", s.issuperset({1})) # Check if the first set is a superset of the second set
+print("Subset:", {1}.issubset(s)) # Check if the first set is a subset of the second set.Checks whether all elements of Set A are present in Set B.
+print("Superset:", s.issuperset({1})) # Check if the first set is a superset of the second set.Checks whether Set A contains all elements of Set B.
 print("Disjoint:", s.isdisjoint({10, 11})) # Check if the two sets have no elements in common
+
+'''
+issubset()    → Small set inside Big set?
+issuperset()  → Big set contains Small set?
+isdisjoint()  → No common elements?
+'''
