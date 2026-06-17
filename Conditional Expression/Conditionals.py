61ae005c6a55f1634cf339af00de5923f0d7cdab
Sayanjit <sayanjitjana373@gmail.com>
2026-06-17 21:22:00 +0530

    Conditional statements are used to perform different actions based on different conditions.

---
+++ Conditional Expression/Conditionals.py
@@ -0,0 +1,19 @@
+''' Conditional statements are used to perform different actions based on different conditions. In Python, we have the following conditional statements:
+1. if statement
+2. if-else statement
+3. if-elif-else statement
+4. nested if statement
+'''
+
+a = int(input("Enter your age: "))
+
+# If else statement
+if(a>=18):
+    print("You are above the age of consent")
+    print("Good for you")
+
+else:
+    print("You are below the age of consent")
+
+
+print("End of Program")
