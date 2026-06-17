42dcaeca048bf8ef29c5bf9356641fe33dc1f517
Sayanjit <sayanjitjana373@gmail.com>
2026-06-17 21:50:19 +0530

    Write a program to find out whether a given post is talking about “Sayanjit” or not.

---
+++ Problem Set/Chapter 6/Problem_7.py
@@ -0,0 +1,11 @@
+# Write a program to find out whether a given post is talking about “Sayanjit” or not.
+
+post = input("Enter the post: ")
+
+
+
+if("sayanjit" in post.lower()):
+    print("This post is talking about sayanjit")
+
+else:
+    print("This post is not talking about sayanjit")
