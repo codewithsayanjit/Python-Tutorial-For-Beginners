349c11272adf81f105139aa37de9651a15d2962a
Sayanjit <sayanjitjana373@gmail.com>
2026-06-18 18:52:40 +0530

    Pass is a null statement in python.

---
+++ Loops/Pass_Statement.py
@@ -0,0 +1,10 @@
+# Pass statement is used when a statement is required syntactically but you do not want any command or code to execute. It is a null statement in Python. The pass statement is used as a placeholder for future code. When the pass statement is executed, nothing happens, but you avoid getting an error when empty code is not allowed.
+
+for i in range(645):
+    pass
+
+
+i = 0
+while(i<45):
+    print(i)
+    i += 1
