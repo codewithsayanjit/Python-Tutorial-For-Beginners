6708640050456e0f86ad7530331883daf7776e67
Sayanjit <sayanjitjana373@gmail.com>
2026-06-18 18:42:36 +0530

    List Using While Loop

---
+++ Loops/List_Using_While.py
@@ -0,0 +1,15 @@
+'''
+Lists are used to store multiple items in a single variable. Lists are one of 4 built-in data types in Python used to store collections of data, the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.
+Lists are created using square brackets.
+'''
+
+
+l = [1, "Sayanjit", False, "This", "Rohan", "Shubham", "Shubhi"]
+
+i = 0
+
+while(i<len(l)):
+    print(l[i])
+    i +=1
+
+    
