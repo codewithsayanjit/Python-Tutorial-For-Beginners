9704e51a15248f143c3f4f726ee038d3cb38b136
Sayanjit <sayanjitjana373@gmail.com>
2026-06-23 23:51:06 +0530

    An attribute that belongs to the Instance (object).

---
+++ Object Oriented Programming/Instance_vs_class_attr.py
@@ -0,0 +1,11 @@
+# An attribute that belongs to the Instance (object).
+
+class Employee: 
+    language = "Python" # This is a class attribute
+    salary = 1200000
+
+
+harry = Employee()
+harry.language = "JavaScript" # This is an instance attribute
+print(harry.language, harry.salary)
+ 
