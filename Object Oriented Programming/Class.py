a4446bd46e79434440ec16a4c72c3a41fd077be3
Sayanjit <sayanjitjana373@gmail.com>
2026-06-24 16:50:39 +0530

    An object is an instantiation of a class. When class is defined, a template (info) is defined. Memory is allocated only after object instantiation.

--- Object Oriented Programming/Class.py
+++ Object Oriented Programming/Class.py
@@ -1,12 +1,28 @@
+'''
+Object Oriented Programming : 
+
+Solving a problem by creating object is one of the most popular approaches in programming. This is called object-oriented programming.
+An object is an instantiation of a class. When class is defined, a template (info) is defined. Memory is allocated only after object instantiation.
+This concept focuses on using reusable code (DRY (Don't Repeat Yourself) Principle).
+
+
+Modelling A Problem In OOPS : 
+
+We identify the following in our problem.
+1)Noun → Class → Employee
+2)Adjective → Attributes → name, age, salary
+3)Verbs → Methods → getSalary(), increment()
+'''
+
 # A class is a blueprint for creating object.
 
 class Employee: 
-    language = "Py" # This is a class attribute
+    language = "Py" # This is a class attribute. An attribute that belongs to the class rather than a particular object.
     salary = 1200000
 
 
 harry = Employee()
-harry.name = "Harry" # This is an instance attribute
+harry.name = "Sayanjit" # This is an instance attribute.An attribute that belongs to the Instance (object).
 print(harry.name, harry.language, harry.salary)
 
 rohan = Employee()
