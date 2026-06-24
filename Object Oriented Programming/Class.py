f4dacbc5aa8c93c651c35285fd64f246701a4bd2
Sayanjit <sayanjitjana373@gmail.com>
2026-06-24 16:52:47 +0530

    An object is an instantiation of a class. When class is defined, a template (info) is defined. Memory is allocated only after object instantiation.

--- Object Oriented Programming/Class.py
+++ Object Oriented Programming/Class.py
@@ -17,16 +17,15 @@ We identify the following in our problem.
 # A class is a blueprint for creating object.
 
 class Employee: 
-    language = "Py" # This is a class attribute. An attribute that belongs to the class rather than a particular object.
+    language = "Python" # This is a class attribute. An attribute that belongs to the class rather than a particular object.
     salary = 1200000
 
-
-harry = Employee()
-harry.name = "Sayanjit" # This is an instance attribute.An attribute that belongs to the Instance (object).
-print(harry.name, harry.language, harry.salary)
+sayanjit = Employee()
+sayanjit.name = "Sayanjit" # This is an instance attribute.An attribute that belongs to the Instance (object).
+print(sayanjit.name, sayanjit.language, sayanjit.salary)
 
 rohan = Employee()
-rohan.name = "Rohan Roro Robinson"
+rohan.name = "Rohan Pradhan"
 print(rohan.name, rohan.salary, rohan.language)
 
 # Here name is instance attribute and salary and language are class attributes as they directly belong to the class
