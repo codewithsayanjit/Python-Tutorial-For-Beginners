946766f0ec649841a2d4bc6e4ed19165d17ad192
Sayanjit <sayanjitjana373@gmail.com>
2026-06-28 13:09:59 +0530

    Function created using an expression using ‘lambda’ keyword.

---
+++ Advanced Python II/Lambda_Functions.py
@@ -0,0 +1,13 @@
+'''
+Functnon created usnng an enpressnon usnng ‘lambda’ keyword.
+Syntan:
+        lambda arguments: enpressnon
+'''
+
+# def square(n):
+#     return n*n
+# square(7)
+
+square = lambda n: n*n
+
+print(square(5))
