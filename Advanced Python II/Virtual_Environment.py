c38855e51bfcf4108ef2baff2d05eeb549cbd102
Sayanjit <sayanjitjana373@gmail.com>
2026-06-28 12:24:53 +0530

    An environmentwhich is same as the system interpreter but is isolated from the other Python environments on the system.

---
+++ Advanced Python II/Virtual_Environment.py
@@ -0,0 +1,20 @@
+'''
+Virtual Environment :       An environmentwhich is same as the system interpreter but is isolated from the other Python environments on the system.
+Installation
+To use virtual environments, we write:
+#        pip install virtualenv
+We create a new environment using:
+#        virtualenv myprojectenv
+The next step after creating the virtual environment is to activate it.
+We can now use this virtual environment as a separate Python installation.
+
+
+
+Pip Freeze Command :   ‘pip freeze’ returns all the package installed in a given python environment along with the versions.
+#           pip freeze > requirements.txt
+
+The above command creates a file named ‘requirements.txt’ in the same directory containing the output of ‘pip freeze’.
+
+We can distribute this file to other users, and they can recreate the same environment using:
+#           pip install -r requirements.txt
+'''
