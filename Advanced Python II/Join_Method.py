2b9dae75477aad88f2b1e0bcc51a3a9c063be7c9
Sayanjit <sayanjitjana373@gmail.com>
2026-06-28 17:36:21 +0530

    Creates a string from iterable objects.

---
+++ Advanced Python II/Join_Method.py
@@ -0,0 +1,15 @@
+# join() Method in Python
+# The join() method combines all elements of an iterable (such as a list)
+# into a single string using the specified separator.
+
+# List of strings
+a = ["Sayanjit", "Rohan", "Shubham"]
+
+# Join all names using "::" as the separator
+final = "::".join(a)
+
+# Print the resulting string
+print(final)
+
+# Output:
+# Sayanjit::Rohan::Shubham
